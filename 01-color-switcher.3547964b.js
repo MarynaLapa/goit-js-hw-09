ab3e311ae797c05e5f65092a6bf34b2cd4931be7
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-10-17 16:22:17 +0000

    Deploying to gh-pages from @ MarynaLapa/goit-js-hw-09@9b660da01752013ab15f0c1bc5382375153257fd 🚀

---
+++ 01-color-switcher.3547964b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),console.log(t),console.log(e)})),e.addEventListener("click",(function(){e.setAttribute("disabled",""),t.removeAttribute("disabled"),clearTimeout(o),console.log(t),console.log(e)}));let o=null;
+//# sourceMappingURL=01-color-switcher.3547964b.js.map
