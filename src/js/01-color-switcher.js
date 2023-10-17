9b660da01752013ab15f0c1bc5382375153257fd
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-10-17 19:22:08 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', handlerStart);
+btnStop.addEventListener('click', handlerStop);
+
+let timerId = null; 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function handlerStart() {
+    btnStart.setAttribute('disabled', '');
+    btnStop.removeAttribute('disabled');
+
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    console.log(btnStart)
+    console.log(btnStop)
+}
+
+function handlerStop() {
+    btnStop.setAttribute('disabled', '');
+    btnStart.removeAttribute('disabled');
+    clearTimeout(timerId);
+    console.log(btnStart)
+    console.log(btnStop)
+}
